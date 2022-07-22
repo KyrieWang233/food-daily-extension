a4feb11d1904163fa746271212b8cd03861fae10
KyrieWang <773647312@qq.com>
2022-07-22 11:20:11 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from "path"
+import { crx3 } from 'vite-plugin-vue-crx3';
+import commonjs from '@rollup/plugin-commonjs';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [commonjs(), vue(), crx3()], // crx3即为插件入口函数
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    target: "es2015",
+    rollupOptions: {
+      input: resolve(__dirname, "src/manifest.json"), // 将源码中的manifest.json作为入口文件
+    },
+  },
+})
